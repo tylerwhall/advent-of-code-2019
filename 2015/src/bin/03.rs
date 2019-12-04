5c1299458f599b3697e7b0742e40658f0cc0fc5d
Tyler Hall <tylerwhall@gmail.com>
2019-12-04 01:43:36 -0500

    2015 Day 3 part 2

--- 2015/src/bin/03.rs
+++ 2015/src/bin/03.rs
@@ -1,8 +1,7 @@
-fn main() {
+fn unique(mut visited: Vec<(isize, isize)>, i: impl Iterator<Item = u8>) -> Vec<(isize, isize)> {
     let mut pos = (0, 0);
-    let mut visited = Vec::new();
 
-    for dir in include_bytes!("03.txt").iter() {
+    for dir in i {
         match dir {
             b'>' => pos.0 += 1,
             b'<' => pos.0 -= 1,
@@ -11,10 +10,20 @@ fn main() {
             _ => panic!("invalid input"),
         }
 
+        // yay linear search
         if !visited.contains(&pos) {
             visited.push(pos);
         }
     }
+    visited
+}
 
-    dbg!(visited.len());
+fn main() {
+    let input = include_bytes!("03.txt");
+    let santa = unique(Vec::new(), input.iter().cloned()).len();
+    dbg!(santa);
+
+    let santa1 = unique(Vec::new(), input.iter().cloned().step_by(2));
+    let santa2 = unique(santa1, input.iter().cloned().skip(1).step_by(2)).len();
+    dbg!(santa2);
 }
